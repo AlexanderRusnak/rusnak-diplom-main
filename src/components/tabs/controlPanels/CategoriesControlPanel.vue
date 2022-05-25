d0f38afee945aafd92f435b2600120e28198918f
= <=>
2022-05-26 00:11:31 +1000

    WIP: разработка таба Categories (готова ControlPanel)

---
+++ src/components/tabs/controlPanels/CategoriesControlPanel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="control-panel">
+    <BaseInput
+      :value="props.search"
+      @input="updateSearch"
+      class="control-panel__search-input"
+      :isOpened="false"
+      type="text"
+      placeholder="Поиск..."
+    />
+    <BaseButton @click="updateFormStatus" size="small-adaptive"
+      >создать категорию</BaseButton
+    >
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+import BaseInput from "@/components/base/BaseInput.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
+
+const props = defineProps(["search"]);
+
+const emit = defineEmits(["updateInput", "updateFormStatus"]);
+
+// ! Updates
+
+const updateSearch = () => {
+  emit("updateSearch", event.target.value);
+};
+
+const updateFormStatus = () => {
+  emit("updateFormStatus");
+};
+</script>
+
+<style lang="scss" scoped>
+.control-panel {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+
+  &__search-input {
+    width: 150px;
+    height: 31px;
+    padding: 0px 10px;
+    font-size: 13px;
+  }
+}
+</style>
--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,24 +1,82 @@
 <template>
   <BaseContainer>
-    <div>Categories View</div>
+    <div class="categories">
+      <CategoriesControlPanel
+        :search="search"
+        @updateSearch="handleSearchChanges"
+        @updateFormStatus="handleFormIsClosedChanges"
+      />
+      <div class="categories__content">
+        <div class="categories__table">Table</div>
+        <div class="categories__edit-form">Edit Form</div>
+      </div>
+    </div>
   </BaseContainer>
 </template>
 
 <script setup>
-import FirebaseService from "@/services/FirebaseService";
+import { ref, onBeforeUnmount } from "vue";
 import { useRouter } from "vue-router";
-import { onBeforeUnmount } from "vue";
+import FirebaseService from "@/services/FirebaseService";
 import BaseContainer from "@/components/base/BaseContainer.vue";
+import CategoriesControlPanel from "@/components/tabs/controlPanels/CategoriesControlPanel.vue";
 
+// ! Init: Refs & Handlers
+
+const search = ref("");
+
+const handleSearchChanges = (newValue) => {
+  search.value = newValue;
+};
+
+const formIsClosed = ref("none");
+
+const handleFormIsClosedChanges = () => {
+  if (formIsClosed.value === "none") {
+    formIsClosed.value = "block";
+  }
+};
+
+// const categoryName = ref("");
+// const loading = ref(false);
+// const categories = ref([]);
+// const selectedCategory = ref("Основная");
+// const isFormatting = ref(false);
+// const selectedObject = ref({});
+
+// ! Init: Router
+
+// создание роутера
 const router = useRouter();
+
+// ! Block: Auth
+
+// проверка авторизации
 const authListener = FirebaseService.checkAuthStateChanged(function (user) {
   if (!user) {
     router.push("/");
   }
 });
+
+// вызов проверки авторизации
 onBeforeUnmount(() => {
   authListener();
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.categories {
+  display: flex;
+  flex-direction: column;
+
+  &__content {
+    display: flex;
+    flex-direction: row;
+    border-top: 1px solid #ccc;
+  }
+
+  &__table {
+    width: 100%;
+  }
+}
+</style>
